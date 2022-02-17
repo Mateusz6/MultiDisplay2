c42480c7362986428d488eba1210acc5595e1b54
Mateusz2 <mateusz.sierakowski@outlook.com>
2022-02-17 23:26:38 +0100

    minor changes

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,42 @@
 #include <Arduino.h>
+#include <Wire.h>
+#include "ADS1X15.h"
+#include "Nextion.h"
+
+ADS1115 ADS(0x48); // sensor type and adress definition
+
+NexPage p0 = NexPage(1,0,"main_page");
+
+NexNumber p0_n0 = NexNumber(1,3,"n0temp");
+NexNumber p0_n1 = NexNumber(1,8,"n1temp");
+
+int temp1 = 0;
+int temp2 = 0;
 
 void setup() {
-  // put your setup code here, to run once:
+
+  //ADS.begin();
+  
+  nexInit(115200L);
+  Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  delay(20);
+
+  // ADS.setGain(0);
+  // int16_t val_0 = ADS.readADC(0);
+  // float f = ADS.toVoltage(1);  // voltage factor
+  // Serial.print("\tAnalog0: "); Serial.print(val_0); Serial.print('\t'); Serial.println(val_0 * f, 3);
+
+  if(temp1 <= 110)
+  {
+    temp1=temp1+2;
+    temp2=temp2+1;
+  }else{
+    temp1=0;
+    temp2=0;
+  }
+  p0_n0.setValue(temp1);
+  p0_n1.setValue(temp2);
 }
